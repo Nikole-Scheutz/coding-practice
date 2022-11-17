8330d570754f0aee48ce78fa55f0a1b7acfe3233
Nikole-Scheutz <nikolai@jinxan.se>
2022-11-17 20:11:28 +0100

    stopped using namespace std and added more functionality to the program

--- cpp/simplelist/main.cpp
+++ cpp/simplelist/main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
+#include <vector>
 
-int main () {
-    std::cout << "Hello world!" << std::endl;
-    return 0;
+void print_menu(std::string name);
+void print_list();
+void add_item();
+void delete_item();
+
+std::vector<std::string> list;
+std::string name;
+
+int main (int arg_count, char *args[]) {
+	if(arg_count > 1) {
+		name = std::string(args[1]);
+		std::cout << "name: ";
+		std::cout << name << std::endl;
+		print_menu(name);
+	}
+	else {
+		std::cout << "Username not supplied... exiting" << std::endl;
+	}
+	return 0;
+}
+
+void print_menu(std::string name) {
+	int choice;
+
+	std::cout << "*********\n";
+	std::cout << " 1 - Print list\n";
+	std::cout << " 2 - Add to list\n";
+	std::cout << " 3 - Delete from list\n";
+	std::cout << " 4 - Quit\n";
+	std::cout << " Enter your choice and press return: ";
+
+	std::cin >> choice;
+
+	if( choice == 4 ){
+		exit(0);
+	}
+	else {
+		std::cout << "Sorry, choice not implemented yet\n";
+	}
 }
